164bc6f6a21fc5a88736d18c4dc24d7f8f516e10
rusanovschi_cristian <rusanovschi1994@gmail.com>
2022-03-24 12:25:06 +0200

    add springAnnotations project

---
+++ src/main/java/com/rusanovschi/springcourse/RockMusic.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rusanovschi.springcourse;
+
+/**
+ *
+ * @author IT-0
+ */
+public class RockMusic implements Music {
+
+    @Override
+    public void getSong() {
+        System.out.println("Will will rock you");
+    }
+}
